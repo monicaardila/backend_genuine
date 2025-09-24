9ffe500bd1d65561c76636f3447f8ba9bf17c5c7
monicaardila <monicayisethardilagudelo@gmail.com>
2025-09-24 02:22:57 -0500

    Fix healthcheck - add simple healthcheck in index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,20 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Simple healthcheck for Railway
+if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/') {
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin');
+    echo json_encode([
+        'status' => 'ok',
+        'message' => 'API is running',
+        'timestamp' => date('c')
+    ]);
+    exit;
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
