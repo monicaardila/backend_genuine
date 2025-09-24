7457caebc0f249bc729d4a6ece55e500c676afd4
monicaardila <monicayisethardilagudelo@gmail.com>
2025-09-24 02:11:32 -0500

    Agregar middleware CORS personalizado para Laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \App\Http\Middleware\CorsMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
